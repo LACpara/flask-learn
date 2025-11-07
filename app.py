b312f4928f47132eb604bc54838acc507a1ddb81
LazyAndCreapy <13694172+lazyandcreapy@user.noreply.gitee.com>
2025-11-07 12:39:17 +0800

    Ready to deploy

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-from watchlist import create_app
+import os
 
-app = create_app()
+from watchlist import create_app
+from dotenv import load_dotenv
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+app = create_app(config_name='production')
